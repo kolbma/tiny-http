bdf9dfbdaba298a18b5d53f531f7d48e9ba64ae8
arlecchino <github@tower-net.de>
2024-01-15 17:13:17 +0100

    chore: ssl feature needs higher memory limit

--- tests/util-equal-reader-rlimit-test.rs
+++ tests/util-equal-reader-rlimit-test.rs
@@ -11,14 +11,14 @@ mod support;
 fn test_equal_reader_drop_rlimit() {
     // this limits need to be fiddled out, because test runs need a lot more memory than the productive server
     rlimit::Resource::AS
-        .set(90_000_000, 90_000_000)
-        .expect("ulimit -v 90_000_000 failed");
+        .set(100_000_000, 100_000_000)
+        .expect("ulimit -v 100_000_000 failed");
 
     let (server, client) = support::new_one_server_one_client();
 
     {
         let mut client = client;
-        (write!(client, "GET / HTTP/1.1\r\nHost: localhost\r\nContent-Type: text/plain; charset=utf8\r\nContent-Length: 104857600\r\n\r\nhello")).unwrap();
+        (write!(client, "GET / HTTP/1.1\r\nHost: localhost\r\nContent-Type: text/plain; charset=utf8\r\nContent-Length: 204857600\r\n\r\nhello")).unwrap();
     }
 
     let mut request = server.recv().unwrap();
