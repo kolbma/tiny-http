296ba2ad67402bae23e70cd3d24fd4794eef9447
arlecchino <github@tower-net.de>
2024-02-03 14:53:58 +0100

    chore: remediate http2/3 version parsing

--- src/common/http_version.rs
+++ src/common/http_version.rs
@@ -2,7 +2,7 @@ use std::{convert::TryFrom, fmt};
 
 use ascii::AsciiStr;
 
-const HTTP_VERSION_HEADER: &[&str] = &["HTTP/0.9", "HTTP/1.0", "HTTP/1.1", "HTTP/2.0", "HTTP/3.0"];
+const HTTP_VERSION_HEADER: &[&str] = &["HTTP/0.9", "HTTP/1.0", "HTTP/1.1", "HTTP/2", "HTTP/3"];
 
 /// HTTP/{version} Request Version (HTTP/1.0 or HTTP/1.1)
 #[derive(Clone, Copy, Debug, Eq, Ord, PartialEq, PartialOrd)]
@@ -13,10 +13,10 @@ pub enum HttpVersion {
     Version1_0,
     /// HTTP/1.1
     Version1_1,
-    /// HTTP/2.0
-    Version2_0,
-    /// HTTP/3.0
-    Version3_0,
+    /// HTTP/2
+    Version2,
+    /// HTTP/3
+    Version3,
 }
 
 impl HttpVersion {
@@ -31,11 +31,11 @@ impl HttpVersion {
 impl std::fmt::Display for HttpVersion {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
         let v = match self {
-            Self::Version0_9 => "0.9",
-            Self::Version1_0 => "1.0",
             Self::Version1_1 => "1.1",
-            Self::Version2_0 => "2.0",
-            Self::Version3_0 => "3.0",
+            Self::Version3 => "3",
+            Self::Version2 => "2",
+            Self::Version1_0 => "1.0",
+            Self::Version0_9 => "0.9",
         };
         f.write_str(v)
     }
@@ -48,10 +48,10 @@ impl TryFrom<(u8, u8)> for HttpVersion {
         match value {
             // ordered for most occurrence
             (1, 1) => Ok(Self::Version1_1),
-            (2, 0) => Ok(Self::Version2_0),
+            (3, 0) => Ok(Self::Version3),
+            (2, 0) => Ok(Self::Version2),
             (1, 0) => Ok(Self::Version1_0),
             (0, 9) => Ok(Self::Version0_9),
-            (3, 0) => Ok(Self::Version3_0),
             _ => Err(HttpVersionError(value.0, value.1)),
         }
     }
@@ -78,9 +78,10 @@ impl TryFrom<&[u8]> for HttpVersion {
 
     fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
         let len = value.len();
-        let (value, len) = if len == 8 && &value[0..5] == b"HTTP/" {
+        let (value, len) = if (len == 8 || len == 6) && &value[0..5] == b"HTTP/" {
             // "HTTP/1.0"
-            (&value[5..], 3)
+            let v = &value[5..];
+            (v, v.len())
         } else {
             (value, len)
         };
@@ -91,7 +92,13 @@ impl TryFrom<&[u8]> for HttpVersion {
             let minor = value[2];
             let range = b'0'..=b'9';
             if range.contains(&major) && range.contains(&minor) {
-                return Self::try_from((major - b'0', minor - b'0'));
+                return Self::try_from((major - 48, minor - 48));
+            }
+        } else if len == 1 {
+            let major = value[0];
+            let range = b'2'..=b'9';
+            if range.contains(&major) {
+                return Self::try_from((major - 48, 0));
             }
         }
 
@@ -107,7 +114,11 @@ impl std::error::Error for HttpVersionError {}
 
 impl std::fmt::Display for HttpVersionError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_fmt(format_args!("unsupported HTTP/{}.{}", self.0, self.1))
+        if self.0 < 2 {
+            f.write_fmt(format_args!("unsupported HTTP/{}.{}", self.0, self.1))
+        } else {
+            f.write_fmt(format_args!("unsupported HTTP/{}", self.0))
+        }
     }
 }
 
@@ -125,13 +136,13 @@ mod tests {
             ("HTTP/0.9", Some(HttpVersion::Version0_9)),
             ("HTTP/1.0", Some(HttpVersion::Version1_0)),
             ("HTTP/1.1", Some(HttpVersion::Version1_1)),
-            ("HTTP/2.0", Some(HttpVersion::Version2_0)),
-            ("HTTP/3.0", Some(HttpVersion::Version3_0)),
+            ("HTTP/2", Some(HttpVersion::Version2)),
+            ("HTTP/3", Some(HttpVersion::Version3)),
             ("0.9", Some(HttpVersion::Version0_9)),
             ("1.0", Some(HttpVersion::Version1_0)),
             ("1.1", Some(HttpVersion::Version1_1)),
-            ("2.0", Some(HttpVersion::Version2_0)),
-            ("3.0", Some(HttpVersion::Version3_0)),
+            ("2", Some(HttpVersion::Version2)),
+            ("3", Some(HttpVersion::Version3)),
             ("HTTP/0.8", None),
             ("HTTP/1.2", None),
             ("HTTP/2.1", None),
