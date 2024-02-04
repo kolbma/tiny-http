7fc61e35cb82c7fb4195f385c48c77d0c40c3f1d
arlecchino <github@tower-net.de>
2024-02-04 13:56:46 +0100

    chore: no write! macro in Display

--- src/common/status_code.rs
+++ src/common/status_code.rs
@@ -81,7 +81,7 @@ impl StatusCode {
 
 impl std::fmt::Display for StatusCode {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
+        f.write_str(&self.0.to_string())
     }
 }
 
